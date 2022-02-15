abdad5b7c808cb17e25db047e45f19236692af70
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-02-15 12:14:50 -0800

    reimplemented redux, added reset feature

--- redux-spotlight/src/index.js
+++ redux-spotlight/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './App';
-import { store } from './store/store';
+
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 render(
   <Provider store={store}>
--- redux-spotlight/src/views/Counter/Counter.js
+++ redux-spotlight/src/views/Counter/Counter.js
@@ -1,31 +1,28 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { decrement, increment } from './counterSlice';
+import { increment, decrement, reset } from './counterSlice';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
-  // The function below is called a selector and allows us to select a value from
-  // the state.
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
 
   return (
     <div>
+      <p>{count}</p>
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        +
+      </button>
       <button
         aria-label="Decrement value"
-        onClick={() => {
-          dispatch(decrement());
-        }}
+        onClick={() => dispatch(decrement())}
       >
         -
       </button>
-      <span>{count}</span>
-      <button
-        aria-label="Increment value"
-        onClick={() => {
-          dispatch(increment());
-        }}
-      >
-        +
+      <button aria-label="Increment value" onClick={() => dispatch(reset())}>
+        reset
       </button>
     </div>
   );
